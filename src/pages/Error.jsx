a0544cca230e3d8054c5f97c3914d29b5ea85a24
belvip <pouadjeubelvi@gmail.com>
2025-01-26 22:22:10 +0100

    error page for 404 and other errors

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -14,6 +14,7 @@ const Error = () => {
                 <div className="text-center">
 
                     <p className="text-9xl font-semibold text-primary">404</p>
+                    
                     <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
                         Page Not Found
                     </h1>
